6b0bcb8b6566f0b93e029f00b0bbd32af743b98a
Anna <anna.zueva.mail@inbox.ru>
2020-10-21 23:28:05 +0300

    Удаляет исключение для линтера

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -3,7 +3,6 @@ import PropTypes from "prop-types";
 import MainPage from "../main-page/main-page";
 
 const App = (props) => {
-  // eslint-disable-next-line react/prop-types
   const {placesCount} = props;
 
   return (
