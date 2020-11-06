d88778e927206851c206f94579d7571cb7c30db6
Anna <anna.zueva.mail@inbox.ru>
2020-11-06 08:54:53 +0300

    исправляет ошибку

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -7,7 +7,7 @@ const Header = (props) => {
   const {sitePage} = props;
 
   const getLogo = (page) => {
-    return (page !== sitePages.Main)
+    return (page === sitePages.MAIN)
       ? (
         <a className="header__logo-link header__logo-link--active">
           <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
