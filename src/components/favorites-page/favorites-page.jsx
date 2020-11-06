f96f9da4a7088e4a95512b2666085f6d8373ef5f
Anna <anna.zueva.mail@inbox.ru>
2020-11-06 06:17:28 +0300

    Переносит блок header в отдельный компонент

--- src/components/favorites-page/favorites-page.jsx
+++ src/components/favorites-page/favorites-page.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import {OfferPropTypes} from "../app/app-prop-types";
 import PlacesList from "../places-list/places-list";
+import Header from "../header/header";
 import {sitePages} from "../../const";
 
 const FavoritesPage = (props) => {
@@ -10,31 +11,7 @@ const FavoritesPage = (props) => {
   return (
     <div className="page">
 
-      <header className="header">
-        <div className="container">
-          <div className="header__wrapper">
-
-            <div className="header__left">
-              <a className="header__logo-link" href="main.html">
-                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
-              </a>
-            </div>
-
-            <nav className="header__nav">
-              <ul className="header__nav-list">
-                <li className="header__nav-item user">
-                  <a className="header__nav-link header__nav-link--profile" href="#">
-                    <div className="header__avatar-wrapper user__avatar-wrapper">
-                    </div>
-                    <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
-                  </a>
-                </li>
-              </ul>
-            </nav>
-
-          </div>
-        </div>
-      </header>
+      <Header />
 
       <main className="page__main page__main--favorites">
         <div className="page__favorites-container container">
