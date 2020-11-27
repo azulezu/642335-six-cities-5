f851478e889d0e8375cbf4f80779246e77272622
Anna <anna.zueva.mail@inbox.ru>
2020-11-27 21:18:30 +0300

    wip: Временно убирает props с общими списками предложений и комментариев из App (страница offer/id не работает, но это сейчас не важно)

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,17 +2,14 @@ import React from "react";
 import PropTypes from "prop-types";
 import {connect} from "react-redux";
 import {ActionCreator} from "../../store/action";
-import OfferPropTypes from "../offer-page/offer.prop";
-import ReviewPropTypes from "../review/review.prop";
+
 import {Switch, Route, BrowserRouter} from "react-router-dom";
 import MainPage from "../main-page/main-page";
 import FavoritesPage from "../favorites-page/favorites-page";
 import LoginPage from "../login-page/login-page";
-import OfferPage from "../offer-page/offer-page";
-import {selectOffersByCity} from "../../core";
 
-const App = (props) => {
-  const {offers, city, changeOffer} = props;
+
+const App = () => {
 
   return (
     <BrowserRouter>
@@ -29,35 +26,20 @@ const App = (props) => {
           <FavoritesPage />
         </Route>
 
-        <Route exact path="/offer/:id?"
-          render={({match}) => {
-            const currentOffer = offers
-              .find((offer) => offer.id === match.params.id) || offers[0];
-            changeOffer(currentOffer);
 
-            return (
-              <OfferPage
-                nearOffers={selectOffersByCity(city, offers).slice(1, 4)}
-              />
-            );
-          }}
-        >
-        </Route>
       </Switch>
     </BrowserRouter>
   );
 };
 
 App.propTypes = {
-  offers: PropTypes.arrayOf(OfferPropTypes).isRequired,
-  reviews: PropTypes.arrayOf(ReviewPropTypes).isRequired,
+
   city: PropTypes.string.isRequired,
   changeOffer: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = (state) => ({
-  offers: state.offers,
-  reviews: state.reviews,
+
   offer: state.offer,
   city: state.city,
 });
