0cbb7fe42d4ed4ea6d6fde56931396032c288e14
Anna <anna.zueva.mail@inbox.ru>
2020-11-27 21:40:29 +0300

    wip: вместе с пред. коммитом

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,7 +1,5 @@
 import React from "react";
-import PropTypes from "prop-types";
-import {connect} from "react-redux";
-import {ActionCreator} from "../../store/action";
+
 
 import {Switch, Route, BrowserRouter} from "react-router-dom";
 import MainPage from "../main-page/main-page";
@@ -32,23 +30,4 @@ const App = () => {
   );
 };
 
-App.propTypes = {
-
-  city: PropTypes.string.isRequired,
-  changeOffer: PropTypes.func.isRequired,
-};
-
-const mapStateToProps = (state) => ({
-
-  offer: state.offer,
-  city: state.city,
-});
-
-const mapDispatchToProps = (dispatch) => ({
-  changeOffer(offer) {
-    dispatch(ActionCreator.changeOffer(offer));
-  },
-});
-
-export {App};
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
