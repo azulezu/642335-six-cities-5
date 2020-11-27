3ecc6cfc21964ae3ac9a99c3ef9ef507095f0695
Anna <anna.zueva.mail@inbox.ru>
2020-11-27 22:56:21 +0300

    Отменяет изменение App (возвращает props c моками)

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,13 +1,18 @@
 import React from "react";
-
-
+import PropTypes from "prop-types";
+import {connect} from "react-redux";
+import {ActionCreator} from "../../store/action";
+import OfferPropTypes from "../offer-page/offer.prop";
+import ReviewPropTypes from "../review/review.prop";
 import {Switch, Route, BrowserRouter} from "react-router-dom";
 import MainPage from "../main-page/main-page";
 import FavoritesPage from "../favorites-page/favorites-page";
 import LoginPage from "../login-page/login-page";
+import OfferPage from "../offer-page/offer-page";
+import {selectOffersByCity} from "../../core";
 
-
-const App = () => {
+const App = (props) => {
+  const {offers, city, changeOffer} = props;
 
   return (
     <BrowserRouter>
@@ -24,10 +29,46 @@ const App = () => {
           <FavoritesPage />
         </Route>
 
+        <Route exact path="/offer/:id?"
+          render={({match}) => {
+            const currentOffer = offers
+              .find((offer) => offer.id === match.params.id) || offers[0];
+            changeOffer(currentOffer);
 
+            return (
+              <OfferPage
+                nearOffers={selectOffersByCity(city, offers).slice(1, 4)}
+              />
+            );
+          }}
+        >
+        </Route>
       </Switch>
     </BrowserRouter>
   );
 };
 
-export default App;
+App.propTypes = {
+  offers: PropTypes.arrayOf(OfferPropTypes).isRequired,
+  reviews: PropTypes.arrayOf(ReviewPropTypes).isRequired,
+  city: PropTypes.string.isRequired,
+  changeOffer: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  offers: state.offers,
+  reviews: state.reviews,
+  offer: state.offer,
+  city: state.city,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  changeOffer(offer) {
+    dispatch(ActionCreator.changeOffer(offer));
+  },
+});
+
+const memoApp = React.memo(App);
+
+export {App};
+export default React.memo(connect(mapStateToProps, mapDispatchToProps)(memoApp));
